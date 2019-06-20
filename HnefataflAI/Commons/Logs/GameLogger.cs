7dad5641875020ecf131cf4543c11018256d5d36
gallorob <gallotta.roberto@gmail.com>
2019-06-20 23:15:41 +0200

    added loggers

---
+++ HnefataflAI/Commons/Logs/GameLogger.cs
@@ -0,0 +1,56 @@
+﻿using HnefataflAI.Commons.Utils;
+using HnefataflAI.Defaults;
+using HnefataflAI.Pieces;
+using System;
+using System.IO;
+
+namespace HnefataflAI.Commons.Logs
+{
+    class GameLogger
+    {
+        public static readonly string FilePath = String.Format(@"./game_{0}.log", DateTime.Now.ToString("MMddyyyyhhmm"));
+        public static void LogMove(int turn, PieceColors pieceColor, Move move)
+        {
+            if (DefaultValues.LOG_GAME)
+            {
+                string toLog = String.Format("[{0}] - {1} moves {2} from {3} to {4}.",
+                    turn,
+                    PieceColorsUtils.GetRoleFromPieceColor(pieceColor),
+                    move.Piece.ToString(),
+                    move.From,
+                    move.To);
+                Log(toLog);
+            }
+        }
+        public static void LogPieceCapture(PieceColors pieceColor, IPiece captured)
+        {
+            if (DefaultValues.LOG_GAME)
+            {
+                string toLog = String.Format("{0} captures piece {1}.",
+                    PieceColorsUtils.GetRoleFromPieceColor(pieceColor),
+                    captured.ToString());
+                Log(toLog);
+            }
+        }
+        internal static void LogDuration(TimeSpan timeSpan)
+        {
+            if (DefaultValues.LOG_GAME)
+            {
+                string toLog = String.Format("Game lasted {0:00}:{1:00}:{2:00}.{3:00}",
+                    timeSpan.Hours,
+                    timeSpan.Minutes,
+                    timeSpan.Seconds,
+                    timeSpan.Milliseconds / 10);
+                Log(toLog);
+            }
+        }
+        public static void Log(string message)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(FilePath, true))
+            {
+                streamWriter.WriteLine(message);
+                streamWriter.Close();
+            }
+        }
+    }
+}
---
+++ HnefataflAI/Commons/Logs/MovesLogger.cs
@@ -0,0 +1,67 @@
+﻿using HnefataflAI.Commons.Utils;
+using HnefataflAI.Defaults;
+using HnefataflAI.Games.GameState;
+using System;
+using System.IO;
+
+namespace HnefataflAI.Commons.Logs
+{
+    public class MovesLogger
+    {
+        public static readonly string FilePath = String.Format(@"./moves_{0}.log", DateTime.Now.ToString("MMddyyyyhhmm"));
+        public static void LogMove(PieceColors player, int depth, Move bestMove, int bestMoveValue, bool isMaximizing, Move move, int moveValue)
+        {
+            if (DefaultValues.LOG_MOVES_EVAL)
+            {
+                string toLog = String.Format("{6} - {0} at depth {1} - Best move: {2} ({3}) - Evaluated move: {4} ({5})",
+                    isMaximizing ? "Max." : "Min",
+                    depth,
+                    bestMove is null ? "null" : bestMove.ToString(),
+                    bestMoveValue,
+                    move.ToString(),
+                    moveValue,
+                    player.ToString());
+                Log(toLog);
+            }
+        }
+        public static void LogMove(Move move, int value)
+        {
+            if (DefaultValues.LOG_MOVES)
+            {
+                string toLog = String.Format("Move {0} with value {1}.",
+                    move.ToString(),
+                    value);
+                Log(toLog);
+            }
+        }
+        public static void LogEvent(PieceColors pieceColors, Status status, bool isMaximizing)
+        {
+            if (DefaultValues.LOG_MOVES_EVAL)
+            {
+                string ToLog = String.Format("{0}; {1} for {2}",
+                    isMaximizing ? "Max." : "Min."
+                    status.Equals(Status.LOSS) ? "loss" : "win",
+                    PieceColorsUtils.GetRoleFromPieceColor(pieceColors));
+                Log(toLog);
+            }
+        }
+        public static void LogPruning(int alpha, int beta)
+        {
+            if (DefaultValues.LOG_MOVES_EVAL)
+            {
+                string toLog = String.Format("Prune: Alpha {0}; Beta {1}",
+                    alpha,
+                    beta);
+                Log(toLog);
+            }
+        }
+        public static void Log(string message)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(FilePath, true))
+            {
+                streamWriter.WriteLine(message);
+                streamWriter.Close();
+            }
+        }
+    }
+}
